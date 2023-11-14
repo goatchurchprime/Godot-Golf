3f329debe76b1cdaac444cf66b2b56cc0634ba0b
muhfringo <sus.mfringo@gmail.com>
2023-11-15 00:04:13 +0100

    level loading system

--- scripts/gui/score_hud.gd
+++ scripts/gui/score_hud.gd
@@ -2,14 +2,5 @@ class_name HUD extends CanvasLayer
 
 @onready var score_label = $Control/PanelContainer/Panel/ScoreLabel
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func update_text(puts):
 	score_label.text = str(puts)
