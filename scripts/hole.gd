e3a73b6f37b59296a6a72a7ee3ad52638b95e745
muhfringo <sus.mfringo@gmail.com>
2023-12-02 11:34:23 +0100

    audience clapping upon winning

--- scripts/hole.gd
+++ scripts/hole.gd
@@ -3,8 +3,10 @@ class_name Hole extends Area3D
 signal game_win
 
 @onready var camera = $SpringArm3D/HoleCamera
+@onready var clapping_audio_player = $ClappingAudioPlayer
 
 func _on_body_entered(body):
 	if body is Golfball:
 		game_win.emit()
 		camera.activate()
+		clapping_audio_player.play()
