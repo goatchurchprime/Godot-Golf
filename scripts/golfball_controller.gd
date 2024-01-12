a821eeff20374a4c9d342a979a669a6d4d811cb2
muhfringo <sus.mfringo@gmail.com>
2024-01-12 12:42:02 +0100

    improved putting particle and moved it to correct parent

--- scripts/golfball_controller.gd
+++ scripts/golfball_controller.gd
@@ -27,8 +27,8 @@ var locked = true
 @onready var camera = $CameraPosition/SpringArm3D/Camera3D
 @onready var spring_arm = $CameraPosition/SpringArm3D
 
-@onready var particle_emitter = $CameraPosition/PuttingGPUParticle
-@onready var putt_audio_player = $CameraPosition/PuttingAudioPlayer
+@onready var particle_emitter = $FollowPosition/PuttingGPUParticle
+@onready var putt_audio_player = $FollowPosition/PuttingAudioPlayer
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
