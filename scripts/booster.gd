c604f8f6bfa234e04780d7e771e1fe845339253e
muhfringo <sus.mfringo@gmail.com>
2024-01-31 20:14:19 +0100

    Reworked booster.gd script

--- scripts/booster.gd
+++ scripts/booster.gd
@@ -1,8 +1,8 @@
-class_name Booster extends Area3D
+class_name Booster extends Node3D
 
-@export_range(0.1, 2) var boost_strength = 2
+@export_range(0.1, 4, 0.1) var boost_strength = 2.0
 
-func _on_body_entered(body):
+func _on_booster_body_entered(body):
 	if body.get_parent() is Golfball:
-		body.get_parent().impulse = Vector3(global_transform.basis.x.normalized()) * boost_strength
+		body.get_parent().impulse = Vector3(global_transform.basis.z.normalized()) * boost_strength
 		print(body.get_parent().impulse)
