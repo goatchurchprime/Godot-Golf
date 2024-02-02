e184cd588cdfdd35b7e4d11c303f8f05fabd6632
muhfringo <sus.mfringo@gmail.com>
2024-02-02 12:39:24 +0100

    Added sound to booster_pad upon activation

--- scripts/booster.gd
+++ scripts/booster.gd
@@ -2,7 +2,10 @@ class_name Booster extends Node3D
 
 @export_range(0.1, 4, 0.1) var boost_strength = 2.0
 
+@onready var boost_sound_player = $BoostAudioPlayer
+
 func _on_booster_body_entered(body):
 	if body.get_parent() is Golfball:
 		body.get_parent().impulse = Vector3(global_transform.basis.z.normalized()) * boost_strength
 		print(body.get_parent().impulse)
+		boost_sound_player.play()
