e1110b02ceb8b435deb747a5993261054c89748e
muhfringo <sus.mfringo@gmail.com>
2024-02-02 13:49:47 +0100

    Teleport overlay same scroll speed per resolution

--- scripts/teleport_overlay.gd
+++ scripts/teleport_overlay.gd
@@ -1,6 +1,6 @@
 class_name TeleportOverlay extends Control
 
-const SCROLL_SPEED = 3500
+const SCROLL_SPEED = 2
 
 @onready var panel = $Panel
 
@@ -12,7 +12,7 @@ func activate():
 
 func _process(delta):
 	if visible:
-		panel.position.x += SCROLL_SPEED * delta
+		panel.position.x += SCROLL_SPEED * delta * get_viewport().get_visible_rect().size.x
 		if panel.position.x > get_viewport().get_visible_rect().size.x:
 			panel.position.x = 0
 			visible = false
