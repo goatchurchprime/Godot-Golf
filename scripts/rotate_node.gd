e22f1261f65cd9b1316de136a8b287dec098c355
muhfringo <sus.mfringo@gmail.com>
2024-02-11 12:41:59 +0100

    Change rotate_node rotation time formula

--- scripts/rotate_node.gd
+++ scripts/rotate_node.gd
@@ -1,15 +1,16 @@
-extends Node3D
+class_name RotateNode extends Node3D
 
-@export_range(-1, 1, 0.1) var rotation_amt = 0.25
+@export_range(-20, 20, 0.5) var seconds_per_rotation = 5
 
 @export var rotate_x_axis : bool
 @export var rotate_y_axis : bool
 @export var rotate_z_axis : bool
 
 func _physics_process(delta):
+	var rotation_amount = (2*PI)/seconds_per_rotation * delta
 	if rotate_x_axis:
-		rotate_x(rotation_amt*delta)
+		rotate_x(rotation_amount)
 	if rotate_y_axis:
-		rotate_y(rotation_amt*delta)
+		rotate_y(rotation_amount)
 	if rotate_z_axis:
-		rotate_z(rotation_amt*delta)
+		rotate_z(rotation_amount)
