af3bb329ed0d73c2afac409e927f3cdb9d568ef9
muhfringo <sus.mfringo@gmail.com>
2024-02-13 22:11:31 +0100

    Replace rigidbody albedo when on fire

--- scripts/fire_particle.gd
+++ scripts/fire_particle.gd
@@ -1,4 +1,20 @@
 class_name FireParticle extends GPUParticles3D
 
+var original_albedo : Color
+var mat : StandardMaterial3D
+
+func _ready():
+	var parent = get_parent_node_3d()
+	for child in parent.get_children():
+		if child is MeshInstance3D:
+			mat = child.get_active_material(0)
+			if mat is StandardMaterial3D:
+				mat.albedo_color
+				original_albedo = mat.albedo_color
+				mat.albedo_color = Color(1,0,0,1)
+	
+
 func _on_timer_timeout():
+	if mat and original_albedo:
+		mat.albedo_color = original_albedo
 	queue_free()
