083ba6da55332999c41097630a9dc038cd6b1c6b
muhfringo <sus.mfringo@gmail.com>
2024-02-22 12:23:00 +0100

    Add 8 channels to audioplayer

--- scenes/golfball_audio_player.gd
+++ scenes/golfball_audio_player.gd
@@ -1,16 +1,48 @@
 class_name GolfballAudioPlayer extends Node3D
 
-enum states {GLIDING, IMPACT, IN_AIR}
-
-var status = states.IN_AIR
-
 @onready var golfball_rigidbody = $".."/Golfball
+
+@export var impact_sounds : Array[AudioStream] = []
+
+@onready var audio_players = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(golfball_rigidbody)
+	get_audio_players()
+	golfball_rigidbody.body_entered.connect(play_audio)
 
+func get_audio_players():
+	audio_players.clear()
+	for child in get_children():
+		if child is AudioStreamPlayer:
+			audio_players.append(child)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_inactive_audio_player():
+	for audio_player in audio_players:
+		if not audio_player.playing:
+			return audio_player
+
+func randomize_audio(audio_player):
+	audio_player.stream = impact_sounds[randi()%impact_sounds.size()]
+
+func play_audio(body):
+	if not collision_conditions_met(body):
+		return
+	
+	var tmp_audio_player = get_inactive_audio_player()
+	# No free audio player, return!
+	if not tmp_audio_player:
+		return
+	
+	randomize_audio(tmp_audio_player)
+	
+	tmp_audio_player.play()
+
+func collision_conditions_met(body):
+	if body is StaticBody3D:
+		if not body.physics_material_override:
+			return false
+		if body.physics_material_override.rough == false:
+			return false
+	return true
