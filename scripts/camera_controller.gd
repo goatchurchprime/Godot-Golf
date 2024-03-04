d63b9787c8b2630564c1e8a1352f0631c151ab29
muhfringo <sus.mfringo@gmail.com>
2024-03-04 22:53:06 +0100

    Camera fov changes depending on speed

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -2,14 +2,26 @@ extends Node3D
 
 const SENSITIVITY = 0.25
 
+const LERP_WEIGHT = 1
+const FOV_OFFSET = 20
+
+const VELOCITY_THRESHOLD = 6
+
 @onready var ball = $"../.."
 @onready var camera = $Camera3D
+@onready var standard_fov = camera.fov
+@onready var moving_camera_fov = standard_fov + FOV_OFFSET
 
 func _input(event):
 	if event is InputEventMouseMotion and not ball.get_is_aiming():
 		get_parent().rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
 		rotate_x(deg_to_rad(-event.relative.y) * SENSITIVITY)
 		rotation.x = clamp(rotation.x, deg_to_rad(-50),deg_to_rad(10))
+
+func _physics_process(delta):
+	var rigidbody_velocity = abs(ball.rigidbody.linear_velocity.length())
+	camera.fov = lerp(camera.fov, standard_fov + rigidbody_velocity, delta * LERP_WEIGHT)
+	camera.fov = clamp(camera.fov, standard_fov, moving_camera_fov)
 
 func get_rotation_basis():
 	return get_parent().transform.basis.z
