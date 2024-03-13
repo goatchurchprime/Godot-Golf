37f8a5704b46e8eb670b4fa545ad14b9a31c8d30
muhfringo <sus.mfringo@gmail.com>
2024-03-13 10:30:44 +0100

    Safely limit putt volume

--- scripts/audio/putting_audio.gd
+++ scripts/audio/putting_audio.gd
@@ -4,4 +4,5 @@ class_name PuttingAudio extends AudioStreamPlayer3D
 
 func _on_golfball_putted():
 	volume_db = golfball.get_hit_strength() - golfball.MAX_STRENGTH * 1.5
+	volume_db = min(volume_db, 0)
 	play()
