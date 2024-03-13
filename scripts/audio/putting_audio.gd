3e8d64c037121d7cb073ba10910ce1f7280c7d70
muhfringo <sus.mfringo@gmail.com>
2024-03-13 10:30:35 +0100

    Separate putting_audio to its own script

---
+++ scripts/audio/putting_audio.gd
@@ -0,0 +1,7 @@
+class_name PuttingAudio extends AudioStreamPlayer3D
+
+@onready var golfball = $"../.."
+
+func _on_golfball_putted():
+	volume_db = golfball.get_hit_strength() - golfball.MAX_STRENGTH * 1.5
+	play()
