e868fc51306101e32ca492f361d4237bdf8fca2b
muhfringo <sus.mfringo@gmail.com>
2024-03-13 14:41:14 +0100

    Change nestled if statement to immediate return

--- scripts/file_fetcher.gd
+++ scripts/file_fetcher.gd
@@ -3,33 +3,44 @@ class_name FileFetcher extends Node
 static func get_scenes_in_path(path):
 	var arr = []
 	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		while true:
-			var tmp = dir.get_next()
-			if tmp != "":
-				var file_path = dir.get_current_dir() + "/" + tmp
-				file_path = trim_suffixes(file_path)
-				arr.append(file_path)
-			else:
-				dir.list_dir_end()
-				break
+	
+	if !dir:
+		return null
+		
+	dir.list_dir_begin()
+	
+	while true:
+		var tmp = dir.get_next()
+		if tmp != "":
+			var file_path = dir.get_current_dir() + "/" + tmp
+			file_path = trim_suffixes(file_path)
+			arr.append(file_path)
+		else:
+			dir.list_dir_end()
+			break
+	
 	return arr
 
 static func get_sounds_in_path(path):
 	var arr = []
 	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		while true:
-			var tmp = dir.get_next()
-			if tmp != "":
-				var file_path = dir.get_current_dir() + "/" + tmp
-				file_path = trim_suffixes(file_path)
-				arr.append(ResourceLoader.load(file_path))
-			else:
-				dir.list_dir_end()
-				break
+	
+	if !dir:
+		return null
+		
+	dir.list_dir_begin()
+	
+	while true:
+		var tmp = dir.get_next()
+		
+		if tmp != "":
+			var file_path = dir.get_current_dir() + "/" + tmp
+			file_path = trim_suffixes(file_path)
+			arr.append(ResourceLoader.load(file_path))
+		else:
+			dir.list_dir_end()
+			break
+	
 	return arr
 
 # Fixes resource loading when game is exported. Trims suffixes remap and import
